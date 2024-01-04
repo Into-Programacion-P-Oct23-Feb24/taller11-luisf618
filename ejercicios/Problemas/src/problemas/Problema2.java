2475468c52f72999d0fff27a27990c1519d50ef0
Luis Cordova <lcordovacarrion@gmail.com>
2024-01-04 17:17:04 -0500

    Funciones y procedimientos

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,17 +5,73 @@
  */
 package problemas;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Problema2 {
 
+    static Scanner entrada = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        entrada.useLocale(Locale.US);
+        int proceso;
+        System.out.println("Ingrese que area desea calcular");
+        proceso = entrada.nextInt();
+        if (proceso == 1) {
+            obtenerAreaCuadrado();
+        } else {
+            if (proceso == 2) {
+                obtenerAreaTriangulo();
+            } else {
+                if (proceso == 3) {
+                    obtenerAreaRectangulo();
+                } else {
+                    System.out.println("Error");
+                }
+            }
+        }
     }
-    
+
+    public static void obtenerAreaCuadrado() {
+        entrada.useLocale(Locale.US);
+        double l;
+        double area;
+        System.out.println("Ingrese el lado del cuadrado");
+        l = entrada.nextDouble();
+        area = l * l * l * l;
+        System.out.println(area);
+    }
+
+    public static void obtenerAreaTriangulo() {
+        entrada.useLocale(Locale.US);
+        double b;
+        double h;
+        double area;
+        System.out.println("Ingrese la base del triangulo");
+        b = entrada.nextDouble();
+        System.out.println("Ingrese la altura del triangulo");
+        h = entrada.nextDouble();
+        area = (b * h) / 2;
+        System.out.println(area);
+    }
+
+    public static void obtenerAreaRectangulo() {
+        entrada.useLocale(Locale.US);
+        double b;
+        double h;
+        double area;
+        System.out.println("Ingrese la base del rectangulo");
+        b = entrada.nextDouble();
+        System.out.println("Ingrese la altura del rectangulo");
+        h = entrada.nextDouble();
+        area = b * h;
+        System.out.println(area);
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package problemas;
-
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
@@ -15,7 +16,39 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double[] nota = new double[4];
+        for (int i = 0; i < nota.length; i++) {
+        System.out.println("Ingrese la nota");
+        nota[i] = entrada.nextDouble();    
+        }
+        for (int i = 0; i < nota.length; i++) {
+            
+        }
+        
     }
-    
+    public static String obtenerPromedio(double[] a){
+        double suma = 0;
+        double promedio;
+        String proCuali;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+        }
+        promedio = suma / 4;
+        if (promedio >=0 && promedio <= 5) {
+            proCuali = "Regular";
+        }else{
+            if (promedio >=5.1 && promedio <= 8) {
+                proCuali = "Bueno";
+            }else{
+                if (promedio >=8.1 && promedio <= 9) {
+                    proCuali = "Muy Bueno";
+                }else{
+                    proCuali = "Sobresaliente";
+                }
+            }
+        }
+        return proCuali;
+    }
 }
